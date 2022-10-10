bfdb75402e47429362cd18694d01f06aa7785f41
Hanz Zhang <hanzzhan@microsoft.com>
2022-10-09 21:00:58 -0700

    update

--- P0004_MedianTwoSortedArrays.cs
+++ P0004_MedianTwoSortedArrays.cs
@@ -15,7 +15,7 @@ namespace NUnitTests
         {
             Console.WriteLine("tests hanz");
             Assert.AreEqual(true, 1 == 1);
-            Assert.AreEqual(4, GetMedianOfSortedArray_BrutalForce(new int[] {1,2,}, new int[] {4,5,6}));
+            Assert.AreEqual(4, findMedianSortedArrays_BinarySearch(new int[] {1,2,}, new int[] {4,5,6}));
         }
         public double GetMedianOfSortedArray_BrutalForce(int[] nums1, int[] nums2)
         {
@@ -47,5 +47,69 @@ namespace NUnitTests
             return r;
 
         }
+
+        //binary search
+        public double findMedianSortedArrays_BinarySearch(int[] A, int[] B)
+        {
+            int m = A.Length;
+            int n = B.Length;
+            if (m > n)
+            { // to ensure m<=n
+                return findMedianSortedArrays_BinarySearch(B, A);
+            }
+            int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+            while (iMin <= iMax)
+            {
+                int i = (iMin + iMax) / 2;
+                int j = halfLen - i;
+                if (i < iMax && B[j - 1] > A[i])
+                {
+                    iMin = i + 1; // i is too small
+                }
+                else if (i > iMin && A[i - 1] > B[j])
+                {
+                    iMax = i - 1; // i is too big
+                }
+                else
+                { // i is perfect
+                    int maxLeft;
+                    if (i == 0)
+                    {
+                        maxLeft = B[j - 1];
+                    }
+                    else if (j == 0)
+                    {
+                        maxLeft = A[i - 1];
+                    }
+                    else
+                    {
+                        maxLeft = Math.Max(A[i - 1], B[j - 1]);
+                    }
+                    if ((m + n) % 2 == 1)
+                    {
+                        return maxLeft;
+                    }
+
+                    int minRight = 0;
+                    if (i == m)
+                    {
+                        minRight = B[j];
+                    }
+                    else if (j == n)
+                    {
+                        minRight = A[i];
+                    }
+                    else
+                    {
+                        minRight = Math.Min(B[j], A[i]);
+                    }
+
+                    return (maxLeft + minRight) / 2.0;
+                }
+            }
+            return 0.0;
+        }
+
+
     }
 }
