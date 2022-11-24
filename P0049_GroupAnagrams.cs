ad2d67647ff5025fe0cf3111181bb581aa38b164
Hanz Zhang <hanzzhan@microsoft.com>
2022-11-24 14:28:11 -0800

    update 49

--- P0049_GroupAnagrams.cs
+++ P0049_GroupAnagrams.cs
@@ -24,5 +24,30 @@ namespace NUnitTests
                 return new List<IList<string>>(group.Values);
             }
         }
+
+        public class Solution2
+        {
+            public IList<IList<string>> GroupAnagrams(string[] strs)
+            {
+                var group = new Dictionary<string, IList<string>>();
+                foreach (var str in strs)
+                {
+                    var hash = GetHash(str);
+                    group[hash] = group.GetValueOrDefault(hash, new List<string>());
+                    group[hash].Add(str);
+                }
+                return new List<IList<string>>(group.Values);
+            }
+
+            public string GetHash(string s)
+            {
+                var counts = new int[26];
+                foreach (char c in s.ToCharArray())
+                {
+                    ++counts[c - 'a'];
+                }
+                return string.Join(".", counts);
+            }
+        }
     }
 }
