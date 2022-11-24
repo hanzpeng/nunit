1f0794ad797236bda1b904cda1ace55039856c9c
Hanz Zhang <hanzzhan@microsoft.com>
2022-11-23 18:38:38 -0800

    update P90

--- P0090_SubsetsII.cs
+++ P0090_SubsetsII.cs
@@ -6,13 +6,13 @@ using System.Text;
 using NUnit.Framework;
 namespace P0090_SubsetsII
 {
-    public class P0078_Subsets
+    public class P0090_Subsets
     {
         [Test]
         public void Test1()
         {
             Assert.AreEqual(1, 1);
-            var result = Subsets(new int[] { 1, 2, 3, 4 });
+            var result = SubsetsWithDup(new int[] { 1, 2, 3, 4 });
 
             foreach (var li in result)
             {
@@ -24,7 +24,7 @@ namespace P0090_SubsetsII
             }
         }
 
-        public IList<IList<int>> Subsets(int[] nums)
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
         {
             var res = new List<IList<int>>();
             if (nums?.Length == 0) return res;
@@ -49,5 +49,42 @@ namespace P0090_SubsetsII
             dfs(nums, res, cur, index + 1);
         }
 
+    }
+
+    public class Solution2
+    {
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            Dictionary<int, int> numsMap = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                numsMap[num] = numsMap.GetValueOrDefault(num, 0) + 1;
+            }
+
+            IList<IList<int>> result = new List<IList<int>>();
+            Dfs(numsMap, new List<int>(numsMap.Keys), new Stack<int>(), result, 0);
+            return result;
+        }
+        void Dfs(Dictionary<int, int> numsMap, List<int> keys, Stack<int> cur, IList<IList<int>> res, int index)
+        {
+            if (index == keys.Count)
+            {
+                res.Add(new List<int>(cur));
+                return;
+            }
+
+            Dfs(numsMap, keys, cur, res, index + 1);
+
+            var curKey = keys[index];
+            var count = numsMap[curKey];
+
+            for (int i = 1; i <= count; i++)
+            {
+                for (int j = 0; j < i; j++) cur.Push(curKey);
+                Dfs(numsMap, keys, cur, res, index + 1);
+                for (int j = 0; j < i; j++) cur.Pop();
+            }
+
+        }
     }
 }
