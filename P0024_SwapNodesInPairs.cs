a03cc5e1ff553e18a99e1b8b6e970f0b45f6b18a
Hanz Zhang <hanzzhan@microsoft.com>
2022-12-04 17:46:57 -0800

    add P0024

---
+++ P0024_SwapNodesInPairs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    internal class P0024_SwapNodesInPairs
+    {
+        public class Solution
+        {
+            public ListNode SwapPairs(ListNode head)
+            {
+                if (head == null) return head;
+                if (head.next == null) return head;
+                var n1 = head;
+                var n2 = n1?.next;
+                var n3 = n2?.next;
+                if (n1 != null) n1.next = SwapPairs(n3);
+                if (n2 != null) n2.next = n1;
+                return n2;
+            }
+
+            public class ListNode
+            {
+                public int val;
+                public ListNode next;
+            }
+        }
+    }
+}
