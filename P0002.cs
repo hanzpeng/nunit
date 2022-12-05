45562709e672277f07083f050eb387c7ecb03720
Hanz Zhang <hanzzhan@microsoft.com>
2022-12-04 17:11:27 -0800

    update

--- P0002.cs
+++ P0002.cs
@@ -15,7 +15,7 @@ namespace NUnitTests
                 return AddTwoNumbers(l1, l2, 0);
             }
 
-            public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry)
+            public ListNode AddTwoNumbers_Recur(ListNode l1, ListNode l2, int carry)
             {
                 if (l1 == null && l2 == null && carry == 0)
                 {
@@ -30,6 +30,37 @@ namespace NUnitTests
                     int total = i1 + i2 + carry;
                     return new ListNode(total % 10, AddTwoNumbers(l1?.next, l2?.next, total / 10));
                 }
+            }
+
+            public ListNode AddTwoNumbers_Iter(ListNode l1, ListNode l2)
+            {
+                ListNode cur = new ListNode();
+                ListNode dummyHead = new ListNode(0, cur);
+                ListNode tail = dummyHead;
+                int carry = 0;
+                while (l1 != null || l2 != null || carry != 0)
+                {
+                    int i1 = 0;
+                    int i2 = 0;
+                    if (l1 != null)
+                    {
+                        i1 = l1.val;
+                        l1 = l1.next;
+                    }
+                    if (l2 != null)
+                    {
+                        i2 = l2.val;
+                        l2 = l2.next;
+                    }
+                    int total = i1 + i2 + carry;
+                    carry = total / 10;
+                    cur.val = total % 10;
+                    cur.next = new ListNode();
+                    tail = cur;
+                    cur = cur.next;
+                }
+                tail.next = null;
+                return dummyHead.next;
             }
 
             public class ListNode
