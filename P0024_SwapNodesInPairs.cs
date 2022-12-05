03f943abf4132b18debfd24697b02fc36abfa835
Hanz Zhang <hanzzhan@microsoft.com>
2022-12-04 18:18:26 -0800

    P0024

--- P0024_SwapNodesInPairs.cs
+++ P0024_SwapNodesInPairs.cs
@@ -21,7 +21,27 @@ namespace NUnitTests
                 if (n2 != null) n2.next = n1;
                 return n2;
             }
-
+            public ListNode SwapPairs_Iter(ListNode head)
+            {
+                if (head == null || head.next == null) return head;
+                var root = head.next;
+                var cur = head;
+                while (cur != null)
+                {
+                    var n1 = cur;
+                    var n2 = n1?.next;
+                    var n3 = n2?.next;
+                    var n4 = n3?.next;
+                    if (n1 != null)
+                    {
+                        n1.next = n3;
+                        if (n4 != null) n1.next = n4;
+                    }
+                    if (n2 != null) n2.next = n1;
+                    cur = n3;
+                }
+                return root;
+            }
             public class ListNode
             {
                 public int val;
