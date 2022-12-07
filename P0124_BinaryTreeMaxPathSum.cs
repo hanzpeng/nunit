37f096120993cb6fdb43df0f15ba7bdf52967534
Hanz Zhang <hanzzhan@microsoft.com>
2022-12-06 20:30:13 -0800

    update

--- P0124_BinaryTreeMaxPathSum.cs
+++ P0124_BinaryTreeMaxPathSum.cs
@@ -26,17 +26,5 @@ namespace NUnitTests
             return Math.Max(0, Math.Max(lSum, rSum) + root.val);
         }
 
-        //public class TreeNode
-        //{
-        //    public int val;
-        //    public TreeNode left;
-        //    public TreeNode right;
-        //    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        //    {
-        //        this.val = val;
-        //        this.left = left;
-        //        this.right = right;
-        //    }
-        //}
     }
 }
