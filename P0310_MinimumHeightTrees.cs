20655b4f79337e0440673783bef8bd2f017db01a
Hanz Zhang <hanzzhan@microsoft.com>
2022-12-15 11:31:34 -0800

    update 310 FindMinHeightTrees

--- P0310_MinimumHeightTrees.cs
+++ P0310_MinimumHeightTrees.cs
@@ -8,8 +8,54 @@ namespace NUnitTests
 {
     internal class P0310_MinimumHeightTrees
     {
+        public class Solution
+        {
+            public IList<int> FindMinHeightTrees(int n, int[][] edges)
+            {
+                var res = new List<int>();
+                var adj = new HashSet<int>[n];
+                var degree = new int[n];
+                var leafQ = new Queue<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    adj[i] = new HashSet<int>();
+                    res.Add(i);
+                }
+                foreach (var edge in edges)
+                {
+                    adj[edge[0]].Add(edge[1]);
+                    adj[edge[1]].Add(edge[0]);
+                    degree[edge[0]]++;
+                    degree[edge[1]]++;
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    if (degree[i] == 1)
+                    {
+                        leafQ.Enqueue(i);
+                    }
+                }
+                while (res.Count > 2)
+                {
+                    for (int i = leafQ.Count; i > 0; i--)
+                    {
+                        var cur = leafQ.Dequeue();
+                        res.Remove(cur);
+                        foreach (var parent in adj[cur])
+                        {
+                            degree[parent]--;
+                            adj[parent].Remove(cur);
+                            if (degree[parent] == 1)
+                            {
+                                leafQ.Enqueue(parent);
+                            }
+                        }
+                    }
+                }
+                return res;
+            }
+        }
         public class Solution_ExcedingTimeList
-
         {
             public IList<int> FindMinHeightTrees(int n, int[][] edges)
             {
