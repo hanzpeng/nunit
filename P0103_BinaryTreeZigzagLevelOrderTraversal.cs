d150950d60a80347d9762f2dd668034b373fada7
Hanz Zhang <hanzzhan@microsoft.com>
2022-12-15 19:26:03 -0800

    P0103_BinaryTreeZigzagLevelOrderTraversal

--- P0103_BinaryTreeZigzagLevelOrderTraversal.cs
+++ P0103_BinaryTreeZigzagLevelOrderTraversal.cs
@@ -8,5 +8,31 @@ namespace NUnitTests
 {
     internal class P0103_BinaryTreeZigzagLevelOrderTraversal
     {
+        public class Solution
+        {
+            public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+            {
+                var res = new List<IList<int>>();
+                if (root == null) return res;
+                var q = new Queue<TreeNode>();
+                q.Enqueue(root);
+                bool leftToRight = true;
+                while (q.Count > 0)
+                {
+                    var list = new LinkedList<int>();
+                    for (int i = q.Count; i > 0; i--)
+                    {
+                        var cur = q.Dequeue();
+                        if (cur.left != null) q.Enqueue(cur.left);
+                        if (cur.right != null) q.Enqueue(cur.right);
+                        if (leftToRight) list.AddLast(cur.val);
+                        else list.AddFirst(cur.val);
+                    }
+                    res.Add(list.ToList());
+                    leftToRight = !leftToRight;
+                }
+                return res;
+            }
+        }
     }
 }
