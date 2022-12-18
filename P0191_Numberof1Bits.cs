1a5464d33881576ae0f0ed64085e8dc9134d5f19
Hanz Zhang <hanzzhan@microsoft.com>
2022-12-18 10:11:34 -0800

    191. Number of 1 Bits

--- P0191_Numberof1Bits.cs
+++ P0191_Numberof1Bits.cs
@@ -8,5 +8,19 @@ namespace NUnitTests
 {
     internal class P0191_Numberof1Bits
     {
+        public class Solution
+        {
+            public int HammingWeight(uint n)
+            {
+                uint count = 0;
+                while (n != 0)
+                {
+                    //count += n % 2;
+                    count += n & 1;
+                    n = n >> 1;
+                }
+                return (int)count;
+            }
+        }
     }
 }
