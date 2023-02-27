8d01048de87cce081160ce56338dcc5d0da46739
Hanz Zhang <hanzzhan@microsoft.com>
2023-02-26 17:59:39 -0800

    P136

---
+++ P0136.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    internal class P0136
+    {
+        public class Solution
+        {
+            public int SingleNumber(int[] nums)
+            {
+                int a = 0;
+                foreach (var x in nums)
+                {
+                    a ^= x;
+                }
+                return a;
+            }
+        }
+    }
+
+}
