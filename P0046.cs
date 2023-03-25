4ce7e5382b2fe68a052d9d072c13377398a22821
Hanz Zhang <hanzzhan@microsoft.com>
2023-03-25 10:54:57 -0700

    update P0046

--- P0046.cs
+++ P0046.cs
@@ -9,31 +9,65 @@ namespace NUnitTests
 {
     internal class P0046
     {
-        public IList<IList<int>> Permute(int[] nums)
+        public class Solution
         {
-            IList<IList<int>> result = new List<IList<int>>();
-            Dfs(nums, new Stack<int>(), new bool[nums.Length], result);
-            return result;
-        }
-        void Dfs(int[] nums, Stack<int> path, bool[] used, IList<IList<int>> result)
-        {
-            if (path.Count == nums.Length)
+            public IList<IList<int>> Permute(int[] nums)
             {
-                result.Add(new List<int>(path));
-                return;
+                IList<IList<int>> result = new List<IList<int>>();
+                Dfs(nums, new Stack<int>(), new bool[nums.Length], result);
+                return result;
+            }
+            void Dfs(int[] nums, Stack<int> path, bool[] used, IList<IList<int>> result)
+            {
+                if (path.Count == nums.Length)
+                {
+                    result.Add(new List<int>(path));
+                    return;
+                }
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        path.Push(nums[i]);
+                        used[i] = true;
+                        Dfs(nums, path, used, result);
+                        used[i] = false;
+                        path.Pop();
+                    }
+                }
             }
 
-            for (int i = 0; i < nums.Length; i++)
+        }
+
+        public class Solution2
+        {
+            public IList<IList<int>> Permute(int[] nums)
             {
-                if (!used[i])
+                var res = new List<IList<int>>();
+                var set = new HashSet<int>(nums);
+                dfs(set, res, new Stack<int>());
+                return res;
+            }
+
+            public void dfs(HashSet<int> set, List<IList<int>> res, Stack<int> cur)
+            {
+                if (set.Count == 0)
                 {
-                    path.Push(nums[i]);
-                    used[i] = true;
-                    Dfs(nums, path, used, result);
-                    used[i] = false;
-                    path.Pop();
+                    res.Add(cur.ToList());
+                    return;
+                }
+                foreach (var v in set)
+                {
+                    cur.Push(v);
+                    var nextSet = new HashSet<int>(set);
+                    nextSet.Remove(v);
+                    dfs(nextSet, res, cur);
+                    cur.Pop();
                 }
             }
         }
     }
+
+
 }
