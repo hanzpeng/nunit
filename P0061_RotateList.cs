4f34013a72a5dd4fbe5b000064fba9304b8b51b9
Hanz Zhang <hanzzhan@microsoft.com>
2023-03-26 21:57:15 -0700

    P0061

--- P0061_RotateList.cs
+++ P0061_RotateList.cs
@@ -12,6 +12,34 @@ namespace NUnitTests
         {
             public ListNode RotateRight(ListNode head, int k)
             {
+                // base cases
+                if (head == null) return null;
+                if (head.next == null) return head;
+
+                // close the linked list into the ring
+                ListNode old_tail = head;
+                int n;
+                for (n = 1; old_tail.next != null; n++)
+                    old_tail = old_tail.next;
+                old_tail.next = head;
+
+                // find new tail : (n - k % n - 1)th node
+                // and new head : (n - k % n)th node
+                ListNode new_tail = head;
+                for (int i = 0; i < n - k % n - 1; i++)
+                    new_tail = new_tail.next;
+                ListNode new_head = new_tail.next;
+
+                // break the ring
+                new_tail.next = null;
+
+                return new_head;
+            }
+        }
+        public class Solution2
+        {
+            public ListNode RotateRight(ListNode head, int k)
+            {
                 if (head == null || head.next == null) return head;
                 var tail = head;
                 while (tail.next != null)
@@ -37,10 +65,12 @@ namespace NUnitTests
                 return newhead;
             }
 
-            public class ListNode
-            {
-                public ListNode next;
-            }
+
+        }
+
+        public class ListNode
+        {
+            public ListNode next;
         }
     }
 }
