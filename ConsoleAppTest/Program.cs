12ba8ca3c3194ff0b55c6f24b9a2ec0f69e33214
HanzHotmail <mshanz@hotmail.com>
2023-06-05 10:46:15 -0700

    P1676

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -6,31 +6,6 @@ using System.Runtime.ConstrainedExecution;
 
 public class Solution
 {
-    public bool BtreeGameWinningMove(TreeNode root, int n, int x)
-    {
-        if (root == null) return false;
-        return BtreeGameWinningMove(root, n, x, root);
-    }
-
-    public bool BtreeGameWinningMove(TreeNode cur, int n, int x, TreeNode p)
-    {
-        if (cur == null) return false;
-        if (cur.val == x)
-        {
-            if (Count(cur.left) > n / 2) return true;
-            if (Count(cur.right) > n / 2) return true;
-            if (p != null && Count(cur) <= n/2) return true;
-            return false;
-        }
-        return BtreeGameWinningMove(cur.left, n, x, cur) || BtreeGameWinningMove(cur.left, n, x, cur);
-    }
-
-    public int Count(TreeNode cur)
-    {
-        if (cur == null) return 0;
-        return 1 + Count(cur.left) + Count(cur.right);
-    }
-
     public class TreeNode
     {
         public int val;
---
+++ P1676-LowestCommonAncestorOfBinaryTreeIV.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    internal class P1676_LowestCommonAncestorOfBinaryTreeIV
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode[] nodes)
+        {
+            var values = nodes.Select(n => n.val).ToList().ToHashSet();
+            return TopNOde(root, values);
+        }
+
+        public TreeNode TopNOde(TreeNode cur, HashSet<int> values)
+        {
+            if (cur == null) return null;
+
+            if (values.Contains(cur.val)) return cur;
+
+            var topLeft = TopNOde(cur.left, values);
+            var topRight = TopNOde(cur.right, values);
+
+            if (topLeft != null && topRight != null) return cur;
+            if (topLeft != null) return topLeft;
+            if (topRight != null) return topRight;
+            return null;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
