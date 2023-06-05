d27a27404fa5f04e1cc5f8821eb26d561bf1d246
HanzHotmail <mshanz@hotmail.com>
2023-06-05 12:52:49 -0700

    update

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,27 +1,4 @@
-﻿
-Console.WriteLine(int.MaxValue);
-Console.WriteLine(int.MinValue);
-Console.WriteLine(int.MaxValue%10);
-Console.WriteLine(int.MinValue % 10);
-
-Console.WriteLine(Reverse(-123));
-
-
-int Reverse(int x)
-{
-    int rev = 0;
-    while (x != 0)
-    {
-        int pop = x % 10;
-        x /= 10;
-        if (rev > int.MaxValue / 10 || (rev == int.MaxValue / 10 && pop > 7)) return 0;
-        if (rev < int.MinValue / 10 || (rev == int.MinValue / 10 && pop < -8)) return 0;
-        rev = rev * 10 + pop;
-    }
-    return rev;
-}
-
-public class Solution
+﻿public class Solution
 {
 
     public class TreeNode
