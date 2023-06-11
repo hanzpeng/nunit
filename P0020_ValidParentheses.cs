f5c1056b0b489851adbfdc0563a5c8fb34540185
HanzHotmail <mshanz@hotmail.com>
2023-06-11 15:24:20 -0700

    P0020_ValidParentheses

---
+++ P0020_ValidParentheses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    internal class P0020_ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var ch in s)
+            {
+                if (ch == '(' || ch == '[' || ch == '{')
+                {
+                    stack.Push(ch);
+                }
+                if (ch == ')' || ch == ']' || ch == '}')
+                {
+                    if (stack.Count == 0) return false;
+                    if (ch == ')' && stack.Pop() != '(') return false;
+                    else if (ch == ']' && stack.Pop() != '[') return false;
+                    else if (ch == '}' && stack.Pop() != '{') return false;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
