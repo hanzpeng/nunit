e3231a3da73243cd1b01706bc7da877cd0bf4bfd
HanzHotmail <mshanz@hotmail.com>
2023-06-11 11:13:11 -0700

    update P0003

--- P0003_LongestSubstring.cs
+++ P0003_LongestSubstring.cs
@@ -10,6 +10,26 @@ namespace NUnitTests
     {
         public int LengthOfLongestSubstring(string s)
         {
+            var map = new HashSet<char>();
+            int l = 0;
+            int r = 0;
+            int maxLen = 0;
+            while (r < s.Length)
+            {
+                while (map.Contains(s[r]))
+                {
+                    map.Remove(s[l]);
+                    l++;
+                }
+                map.Add(s[r]);
+                maxLen = Math.Max(r - l + 1, maxLen);
+                r++;
+            }
+            return maxLen;
+        }
+
+        public int LengthOfLongestSubstring_hashset(string s)
+        {
             var set = new HashSet<char>();
             int l = 0;
             int r = 0;
