3d2b36215cc3b9fbef3fabe9e04d3c0e27c007da
HanzHotmail <mshanz@hotmail.com>
2023-06-13 11:28:06 -0700

    update

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,7 +1,1 @@
-﻿var list = new List<int>(new int[] {1,2,3,4,5,6});
-var avg = list.Average();
-var min = list.Min();
-var max = list.Max();
-Console.WriteLine(min.ToString());
-Console.WriteLine(avg.ToString());
-Console.WriteLine(max.ToString());
+﻿Console.WriteLine("hanz");
