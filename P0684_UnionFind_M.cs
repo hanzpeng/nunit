29763a60d105b451afb2ec18a7598894d37815b5
Hanzhong Zhang <hanzhong.zhang@aboutgolf.com>
2023-09-01 15:39:06 -0700

    update

--- P0684_UnionFind_M.cs
+++ P0684_UnionFind_M.cs
@@ -10,58 +10,58 @@ namespace NUnitTests
     {
         public class Solution
         {
-            public int[] FindRedundantConnection(int[][] edges)
-            {
-                var par = new int[edges.Length + 1];
-                var rank = new int[edges.Length + 1];
-                init(par, rank);
-                foreach (var edge in edges)
-                {
-                    if (Union(edge[0], edge[1], par, rank) == false)
-                    {
-                        return edge;
-                    }
-                }
-                return null;
-            }
+public int[] FindRedundantConnection(int[][] edges)
+{
+    var par = new int[edges.Length + 1];
+    var rank = new int[edges.Length + 1];
+    Init(par, rank);
+    foreach (var edge in edges)
+    {
+        if (Union(edge[0], edge[1], par, rank) == false)
+        {
+            return edge;
+        }
+    }
+    return null;
+}
 
-            void init(int[] par, int[] rank)
-            {
-                for (int i = 0; i < par.Length; i++)
-                {
-                    par[i] = i;
-                    rank[i] = 1;
-                }
-            }
-            bool Union(int p1, int p2, int[] par, int[] rank)
-            {
-                p1 = Find(p1, par);
-                p2 = Find(p2, par);
-                if (p1 == p2) return false;
+void Init(int[] par, int[] rank)
+{
+    for (int i = 0; i < par.Length; i++)
+    {
+        par[i] = i;
+        rank[i] = 1;
+    }
+}
+bool Union(int p1, int p2, int[] par, int[] rank)
+{
+    p1 = Find(p1, par);
+    p2 = Find(p2, par);
+    if (p1 == p2) return false;
 
-                if (rank[p1] > rank[p2])
-                { //union by rank
-                    par[p2] = p1;
-                    rank[p1] += rank[p2];
-                }
-                else
-                {
-                    par[p1] = p2;
-                    rank[p2] += rank[p1];
-                }
-                return true;
-            }
+    if (rank[p1] > rank[p2])
+    { //union by rank
+        par[p2] = p1;
+        rank[p1] += rank[p2];
+    }
+    else
+    {
+        par[p1] = p2;
+        rank[p2] += rank[p1];
+    }
+    return true;
+}
 
-            int Find(int p, int[] par)
-            {
-                while (par[p] != p)
-                {
-                    //path compression
-                    par[p] = par[par[p]];
-                    p = par[p];
-                }
-                return p;
-            }
+int Find(int p, int[] par)
+{
+    while (par[p] != p)
+    {
+        //path compression
+        par[p] = par[par[p]];
+        p = par[p];
+    }
+    return p;
+}
         }
     }
 }
