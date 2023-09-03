f45b53d9a23948231c59728b756935153a500b3c
Hanzhong Zhang <hanzhong.zhang@aboutgolf.com>
2023-09-02 22:30:09 -0700

    460

--- P0460_LFU_Cache.cs
+++ P0460_LFU_Cache.cs
@@ -10,112 +10,124 @@ namespace NUnitTests
     {
         public class LFUCache
         {
-            public class Node
-            {
-                public int key;
-                public int val;
-                public int freq;
-                public Node next;
-                public Node prev;
-                public Node(int key, int val, int freq)
-                {
-                    this.key = key;
-                    this.val = val;
-                    this.freq = freq;
-                }
-                public Node() { }
-            }
-
-            public class DLL
-            {
-                Node head = new();
-                Node tail = new();
-                public DLL()
-                {
-                    this.head.next = tail;
-                    this.tail.prev = head;
-                }
-                public bool IsEmpty()
-                {
-                    return head.next == tail;
-                }
-                public void Enqueue(Node curr)
-                {
-                    curr.prev = tail.prev;
-                    curr.next = tail;
-                    tail.prev.next = curr;
-                    tail.prev = curr;
-                }
-
-                public Node Remove(Node curr)
-                {
-                    curr.prev.next = curr.next;
-                    curr.next.prev = curr.prev;
-                    return curr;
-                }
-
-                public Node Dequeue()
-                {
-                    if (head.next == tail) return null;
-                    return Remove(head.next);
-                }
-            }
-
-            private int capacity;
-            private Dictionary<int, DLL> freqMap;
-            private Dictionary<int, Node> keyMap;
-            private int minFreq = 0;
+            Dictionary<int, Node> _keyToNode;
+            Dictionary<int, Dll> _freqToDll;
+            int _capacity = 0;
+            int _minFreq = 0;
 
             public LFUCache(int capacity)
             {
-                this.capacity = capacity;
-                this.freqMap = new();
-                this.keyMap = new();
-            }
-
-            public void IncreaseFreq(Node curr)
-            {
-                freqMap[curr.freq].Remove(curr);
-                if (freqMap[curr.freq].IsEmpty())
-                {
-                    freqMap.Remove(curr.freq);
-                    if (curr.freq == minFreq) minFreq++;
-                }
-                curr.freq++;
-                freqMap[curr.freq] = freqMap.GetValueOrDefault(curr.freq, new DLL());
-                freqMap[curr.freq].Enqueue(curr);
+                _keyToNode = new Dictionary<int, Node>();
+                _freqToDll = new Dictionary<int, Dll>();
+                _capacity = capacity;
             }
 
             public int Get(int key)
             {
-                if (keyMap.ContainsKey(key))
+                if (_keyToNode.ContainsKey(key))
                 {
-                    IncreaseFreq(keyMap[key]);
-                    return keyMap[key].val;
-                }
-                return -1;
-            }
-
-            public void Put(int key, int val)
-            {
-                if (capacity == 0) return;
-                if (keyMap.ContainsKey(key))
-                {
-                    keyMap[key].val = val;
-                    IncreaseFreq(keyMap[key]);
+                    IncreaseFreq(key);
+                    return _keyToNode[key].Val;
                 }
                 else
                 {
-                    if (keyMap.Count == capacity)
-                    {
-                        keyMap.Remove(freqMap[minFreq].Dequeue().key);
-                        if (freqMap[minFreq].IsEmpty()) freqMap.Remove(minFreq);
-                    }
-                    minFreq = 1;
-                    keyMap[key] = new Node(key, val, 1);
-                    freqMap[1] = freqMap.GetValueOrDefault(1, new DLL());
-                    freqMap[1].Enqueue(keyMap[key]);
+                    return -1;
                 }
+            }
+
+            public void Put(int key, int value)
+            {
+                if (_keyToNode.ContainsKey(key))
+                {
+                    IncreaseFreq(key);
+                    _keyToNode[key].Val = value;
+                }
+                else
+                {
+                    if (_keyToNode.Count == _capacity)
+                    {
+                        // remove least used node
+                        var minFreqDll = _freqToDll[_minFreq];
+                        var firstNode = minFreqDll.RemoveFirstNode();
+                        if (minFreqDll.IsEmpty())
+                        {
+                            _freqToDll.Remove(_minFreq);
+                        }
+                        _keyToNode.Remove(firstNode.Key);
+                    }
+                    var newNode = new Node(key, value, 1);
+                    _freqToDll[1] = new Dll();
+                    _freqToDll[1].AppendNode(newNode);
+                    _keyToNode[key] = newNode;
+                    _minFreq = 1;
+                }
+            }
+
+
+            void IncreaseFreq(int key)
+            {
+                var n = _keyToNode[key];
+                _freqToDll[n.Freq].RemoveNode(n);
+                if (_freqToDll[n.Freq].IsEmpty())
+                {
+                    _freqToDll.Remove(n.Freq);
+                    if (n.Freq == _minFreq)
+                    {
+                        _minFreq++;
+                    }
+                }
+                _freqToDll[n.Freq + 1] = _freqToDll.GetValueOrDefault(n.Freq + 1, new Dll());
+                _freqToDll[n.Freq + 1].AppendNode(n);
+                n.Freq++;
+            }
+        }
+
+        class Node
+        {
+            public int Freq;
+            public int Key;
+            public int Val;
+            public Node Next;
+            public Node Prev;
+            public Node() { }
+            public Node(int key, int val, int freq) { Key = key; Val = val; Freq = freq; }
+        }
+
+        class Dll
+        {
+            public Node Head = new();
+            public Node Tail = new();
+            public Dll()
+            {
+                Head.Next = Tail;
+                Tail.Prev = Head;
+            }
+            public bool IsEmpty() => Head.Next == Tail && Tail.Prev == Head;
+            public void AppendNode(Node n)
+            {
+                n.Prev = Tail.Prev;
+                n.Next = Tail;
+
+                Tail.Prev.Next = n;
+                Tail.Prev = n;
+            }
+
+            public Node RemoveNode(Node n)
+            {
+                n.Prev.Next = n.Next;
+                n.Next.Prev = n.Prev;
+                n.Prev = null;
+                n.Next = null;
+                return n;
+            }
+
+            public Node RemoveFirstNode()
+            {
+                if (IsEmpty())
+                {
+                    return null;
+                }
+                return RemoveNode(Head.Next);
             }
         }
     }
