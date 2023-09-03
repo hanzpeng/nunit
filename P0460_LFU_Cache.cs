6b6c983f4bfea99ea74521c96896ceb009154d8a
Hanzhong Zhang <hanzhong.zhang@aboutgolf.com>
2023-09-02 23:38:47 -0700

    update

--- P0460_LFU_Cache.cs
+++ P0460_LFU_Cache.cs
@@ -283,14 +283,14 @@ namespace NUnitTests
         public class LFUCache
         {
             Dictionary<int, Node> _keyToNode;
-            Dictionary<int, LinkedList<Node>> _freqToDll;
+            Dictionary<int, LinkedList<Node>> _freqToList;
             int _capacity = 0;
             int _minFreq = 0;
 
             public LFUCache(int capacity)
             {
                 _keyToNode = new Dictionary<int, Node>();
-                _freqToDll = new Dictionary<int, LinkedList<Node>>();
+                _freqToList = new Dictionary<int, LinkedList<Node>>();
                 _capacity = capacity;
             }
 
@@ -319,18 +319,18 @@ namespace NUnitTests
                     if (_keyToNode.Count == _capacity)
                     {
                         // remove least used node
-                        var minFreqDll = _freqToDll[_minFreq];
+                        var minFreqDll = _freqToList[_minFreq];
                         var firstNode = minFreqDll.First();
                         minFreqDll.RemoveFirst();
                         if (minFreqDll.Count == 0)
                         {
-                            _freqToDll.Remove(_minFreq);
+                            _freqToList.Remove(_minFreq);
                         }
                         _keyToNode.Remove(firstNode.Key);
                     }
                     var newNode = new Node(key, value, 1);
-                    _freqToDll[1] = new LinkedList<Node>();
-                    _freqToDll[1].AddLast(newNode);
+                    _freqToList[1] = new LinkedList<Node>();
+                    _freqToList[1].AddLast(newNode);
                     _keyToNode[key] = newNode;
                     _minFreq = 1;
                 }
@@ -340,17 +340,17 @@ namespace NUnitTests
             void IncreaseFreq(int key)
             {
                 var n = _keyToNode[key];
-                _freqToDll[n.Freq].Remove(n);
-                if (_freqToDll[n.Freq].Count == 0)
+                _freqToList[n.Freq].Remove(n);
+                if (_freqToList[n.Freq].Count == 0)
                 {
-                    _freqToDll.Remove(n.Freq);
+                    _freqToList.Remove(n.Freq);
                     if (n.Freq == _minFreq)
                     {
                         _minFreq++;
                     }
                 }
-                _freqToDll[n.Freq + 1] = _freqToDll.GetValueOrDefault(n.Freq + 1, new LinkedList<Node>());
-                _freqToDll[n.Freq + 1].AddLast(n);
+                _freqToList[n.Freq + 1] = _freqToList.GetValueOrDefault(n.Freq + 1, new LinkedList<Node>());
+                _freqToList[n.Freq + 1].AddLast(n);
                 n.Freq++;
             }
         }
