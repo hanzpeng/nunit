3bc28e0875bba437890730e471f6d96bbc8e1ef2
hanz <mshanz@hotmail.com>
2024-10-07 14:40:16 -0700

    NumIslands

--- P0200_P0200_NumberOfIslands.cs
+++ P0200_P0200_NumberOfIslands.cs
@@ -47,4 +47,61 @@ namespace NUnitTests
             }
         }
     }
+
+    public class Solution2
+    {
+        public int NumIslands(char[][] grid)
+        {
+            var result = 0;
+            int R = grid.Length;
+            int C = grid[0].Length;
+            var visited = new bool[R][];
+            for (int r = 0; r < R; r++)
+            {
+                visited[r] = new bool[C];
+            }
+            for (var r = 0; r < R; r++)
+            {
+                for (var c = 0; c < C; c++)
+                {
+                    if (grid[r][c] == '1' &&
+                        visited[r][c] == false)
+                    {
+                        result++;
+                        Dfs(r, c, grid, visited, R, C);
+                    }
+                }
+            }
+            return result;
+        }
+        private void Dfs(
+                        int r,
+                        int c,
+                        char[][] grid,
+                        bool[][] visited,
+                        int R,
+                        int C)
+        {
+            visited[r][c] = true;
+            var drdc = new int[][] {
+            new int[] { -1,  0 },
+            new int[] {  1,  0 },
+            new int[] {  0, -1 },
+            new int[] {  0,  1 } };
+            for (int i = 0; i < 4; i++)
+            {
+                var r1 = r + drdc[i][0];
+                var c1 = c + drdc[i][1];
+                if (r1 >= 0 &&
+                    r1 < R &&
+                    c1 >= 0 &&
+                    c1 < C &&
+                    visited[r1][c1] == false &&
+                    grid[r1][c1] == '1')
+                {
+                    Dfs(r1, c1, grid, visited, R, C);
+                }
+            }
+        }
+    }
 }
