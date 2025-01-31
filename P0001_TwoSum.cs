219751ca98ca03345ddaf61ac93375bac9202e73
hanz <mshanz@hotmail.com>
2025-01-31 10:39:50 -0800

    001

--- P0001_TwoSum.cs
+++ P0001_TwoSum.cs
@@ -12,14 +12,14 @@ namespace NUnitTests
         {
             public int[] TwoSum(int[] nums, int target)
             {
-                var seen = new Dictionary<int, int>();
+                var numIndex = new Dictionary<int, int>();
                 for (int i = 0; i < nums.Length; i++)
                 {
-                    if (seen.ContainsKey(target - nums[i]))
+                    if (numIndex.ContainsKey(target - nums[i]))
                     {
-                        return new int[] { seen[target - nums[i]], i };
+                        return new int[] { numIndex[target - nums[i]], i };
                     }
-                    seen[nums[i]] = i;
+                    numIndex[nums[i]] = i;
                 }
                 return null;
             }
