2b9cda96ab63909ba7fba2cb6d302ee6c49f198d
hanz <mshanz@hotmail.com>
2025-02-01 22:39:50 -0800

    p0010

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,29 +1,14 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 Console.WriteLine("hi");
 
-int[] nums = [1, 2, 3, 4, 5, 6, 7];
-var x = Array.BinarySearch(nums,3);
-Console.WriteLine(Bs(nums,4));
+var ss = "a";
+var s0 = ss.Substring(0);
+Console.WriteLine(s0);
+var s1 = ss.Substring(1);
+Console.WriteLine(s1 == "");
 
-
-int Bs(int[] nums, int x) {
-    int l = 0;
-    int r = nums.Length - 1;
-    while (l <= r) {
-        var m = (l + r) / 2;
-        if (nums[m] < x) {
-            r = m - 1;
-        } else if (nums[m] > x) {
-            l = m + 1;
-        } else {
-            return m;
-        }
-        l++;
-        r--;
-    }
-    return -1;
-}
 
---
+++ P0010_.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    internal class P0010
+    {
+        public class Solution {
+            public bool IsMatch(string s, string p) {
+                if (s.Length == 0) {
+                    if (p.Length == 0)
+                        return true;
+                    else {
+                        if (MustMatch(p)) return false;
+                        else return true;
+                    }
+                } else { // s.Length > 0
+                    if (p.Length == 0) return false;
+
+                    if (p.Length > 1 && p[1] == '*') {
+                        // drop c*
+                        if (IsMatch(s, p.Substring(2))) {
+                            return true;
+                        }
+
+                        // skip s0
+                        if ((s[0] == p[0] || p[0] == '.') && IsMatch(s.Substring(1), p)) {
+                            return true;
+                        }
+
+                        return false;
+                    } else {
+                        // regular match for c or .
+                        if (p[0] == '.' || s[0] == p[0]) return IsMatch(s.Substring(1), p.Substring(1));
+                        else return false;
+                    }
+                }
+            }
+
+            bool MustMatch(string p) {
+                int i = 0;
+                while (i < p.Length) {
+                    if (i + 1 < p.Length && p[i + 1] == '*') {
+                        i++;
+                        i++;
+                        continue;
+                    }
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
