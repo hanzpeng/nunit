a7f4f937228e730a66d06967557e2530054d8baf
hanz <mshanz@hotmail.com>
2025-02-04 16:19:45 -0800

    20

--- P0020_ValidParentheses.cs
+++ P0020_ValidParentheses.cs
@@ -4,21 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NUnitTests
-{
-    internal class P0020_ValidParentheses
-    {
-        public bool IsValid(string s)
-        {
+namespace NUnitTests {
+    internal class P0020_ValidParentheses {
+        public class Solution {
+            public bool IsValid(string s) {
+                var pair = new Dictionary<char, char>{
+                                                        {'{', '}'},
+                                                        {'(', ')'},
+                                                        {'[', ']'}
+                                                    };
+                var stack = new Stack<char>();
+                foreach (var c in s) {
+                    if (pair.Keys.Contains(c)) {
+                        stack.Push(c);
+                    } else if (pair.Values.Contains(c)) {
+                        if (stack.Count == 0) {
+                            return false;
+                        }
+                        if (c != pair[stack.Pop()]) return false;
+                    }
+                }
+                return stack.Count == 0;
+            }
+        }
+
+        public bool IsValid(string s) {
             var stack = new Stack<char>();
-            foreach (var ch in s)
-            {
-                if (ch == '(' || ch == '[' || ch == '{')
-                {
+            foreach (var ch in s) {
+                if (ch == '(' || ch == '[' || ch == '{') {
                     stack.Push(ch);
                 }
-                if (ch == ')' || ch == ']' || ch == '}')
-                {
+                if (ch == ')' || ch == ']' || ch == '}') {
                     if (stack.Count == 0) return false;
                     if (ch == ')' && stack.Pop() != '(') return false;
                     else if (ch == ']' && stack.Pop() != '[') return false;
