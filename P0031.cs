2508de2c3509efb8255eacf3e7e81abf48d20dbb
hanz <mshanz@hotmail.com>
2025-02-06 12:30:10 -0800

    31

---
+++ P0031.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    internal class P0031
+    {
+        public class Solution {
+            public void NextPermutation(int[] nums) {
+                int i = nums.Length - 2;
+                while (i >= 0 && nums[i + 1] <= nums[i]) {
+                    i--;
+                }
+
+                if (i >= 0) {
+                    int j = nums.Length - 1;
+                    while (nums[j] <= nums[i]) {
+                        j--;
+                    }
+
+                    Swap(nums, i, j);
+                }
+
+                Reverse(nums, i + 1);
+            }
+
+            private void Reverse(int[] nums, int start) {
+                int i = start, j = nums.Length - 1;
+                while (i < j) {
+                    Swap(nums, i, j);
+                    i++;
+                    j--;
+                }
+            }
+
+            private void Swap(int[] nums, int i, int j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+}
